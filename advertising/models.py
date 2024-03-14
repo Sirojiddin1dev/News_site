1c178e1b373e2b6654329c84f61fe1837ffeec28
salohiddin674 <salohiddinzokirjonov819@gmail.com>
2024-03-14 11:06:29 +0500

    salohiddin

--- advertising/models.py
+++ advertising/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Advertising(models.Model):
     img = models.ImageField(upload_to='advertising_photo')
     end_time = models.DateTimeField()
+    link = models.CharField(max_lenght=200)
     STATUS_CHOICES = (
         ('public','public'),
         ('drop','drop'),
